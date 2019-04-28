a472474e79f3aa1d36debe024730eac8ed6280a7
Varvara <Gudanova>
2019-04-28 20:48:59 +0300

    trik configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,70 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+var changeConfiguration : vscode.Disposable;
+
+var currentPort : number;
+var currentAddress : string;
+
+// Получение текущей конфигурации из настроек пользователя раздела Trik Robot.
+const getConfiguration = () => {
+	var trikConfiguration = vscode.workspace.getConfiguration('Trik Robot');
+
+	var configuration = {
+		port : trikConfiguration.get<number>('port'),
+		address : trikConfiguration.get<string>('address')
+	}
+
+	return configuration;
+}
+
+// Инициализация данных о роботе на основе полученных настроек пользователя.
+const intiRobotData = () => {
+	var configuration = getConfiguration();
+
+	if (configuration.port == undefined)
+	{
+		console.log("Номер порта не объявлен!");
+		return;
+	}
+	
+	if (configuration.address == undefined)
+	{
+		console.log("Адрес не объявлен!");
+		return;
+	}
+
+	currentPort = configuration.port;
+	currentAddress = configuration.address;
+}
+
+// Данных метод вызывается при изменении пользователем настроек конфигурации в разделе TRIK Robot.
+const onConfigurationChange = () => {
+	var newConfiguration = getConfiguration();
+
+	if (newConfiguration.port != currentPort && newConfiguration.port != undefined)
+	{
+		currentPort = newConfiguration.port;
+	}
+
+	if (newConfiguration.address != currentAddress && newConfiguration.address != undefined)
+	{
+		currentAddress = newConfiguration.address;
+	}
+}
+
+// Данный метод вызывается при активации расширения.
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "trikextension" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "trikextension" is now active!');
+	intiRobotData();
+	changeConfiguration = vscode.workspace.onDidChangeConfiguration(onConfigurationChange);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	context.subscriptions.push(vscode.commands.registerCommand('extension.runTrikPlugin', () => {
 		vscode.window.showInformationMessage('Active!');
 	}));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+// Данный метод вызывается при отключении расширения.
+export function deactivate() {
+	changeConfiguration.dispose();
+}
